2663e0c5e43fdf3a6aebd4c08a0cc4b85eb4e33c
Kazimir <KazimirS46@gmail.com>
2022-11-27 16:42:25 +0400

    Create Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,17 @@
-import { useContext } from 'react';
-import { CreateProduct } from './components/CreateProduct';
-import { ErrorMessage } from './components/ErrorMessage';
-import { Loader } from './components/Loader';
-import { Modal } from './components/Modal';
-import { Product } from './components/Product';
-import { ModalContext } from './context/ModalContext';
-import { useProducts } from './hooks/productsHook';
-import { IProduct } from './models';
+import { Route, Routes } from 'react-router-dom';
+import { Navigation } from './components/Navigation';
+import { AboutPage } from './Pages/AboutPage';
+import { ProductPage } from './Pages/ProductsPage';
 
 function App() {
-  const { loading, products, error, addProduct } = useProducts();
-  const { modal, open, close } = useContext(ModalContext);
-
-  const createHandler = (product: IProduct) => {
-    close();
-    addProduct(product);
-  };
-
   return (
-    <div className="container mx-auto max-w-2xl pt-5">
-      {loading && <Loader />}
-      {error && <ErrorMessage error={error} />}
-      {products.map((product) => (
-        <Product product={product} key={product.id} />
-      ))}
-      {modal && (
-        <Modal title="Create New Product" onClose={() => close()}>
-          <CreateProduct onCreate={createHandler} />
-        </Modal>
-      )}
-      <button
-        className="fixed bottom-5 right-5 rounded bg-red-700 text-white text-2xl py-2 px-4"
-        onClick={() => open()}
-      >
-        Add Item
-      </button>
-    </div>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<ProductPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Pages/AboutPage.tsx
@@ -0,0 +1,12 @@
+export function AboutPage() {
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex veritatis
+      maxime ad ut dolor reprehenderit cupiditate ullam, vel, quod ea magnam
+      exercitationem illo adipisci repellendus expedita voluptatibus asperiores,
+      consectetur quasi aspernatur? Veritatis iste alias nisi enim. Autem
+      doloribus nam minima tempore velit sint similique fuga, suscipit
+      necessitatibus exercitationem, quas saepe.
+    </div>
+  );
+}

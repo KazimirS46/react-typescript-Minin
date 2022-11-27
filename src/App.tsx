d6b35b52b206d4dca1b7632e01907c079916f3c7
Kazimir <KazimirS46@gmail.com>
2022-11-27 16:12:34 +0400

    Create Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { useContext } from 'react';
 import { CreateProduct } from './components/CreateProduct';
 import { ErrorMessage } from './components/ErrorMessage';
 import { Loader } from './components/Loader';
 import { Modal } from './components/Modal';
 import { Product } from './components/Product';
+import { ModalContext } from './context/ModalContext';
 import { useProducts } from './hooks/productsHook';
+import { IProduct } from './models';
 
 function App() {
-  const { loading, products, error } = useProducts();
+  const { loading, products, error, addProduct } = useProducts();
+  const { modal, open, close } = useContext(ModalContext);
+
+  const createHandler = (product: IProduct) => {
+    close();
+    addProduct(product);
+  };
 
   return (
     <div className="container mx-auto max-w-2xl pt-5">
@@ -15,9 +24,17 @@ function App() {
       {products.map((product) => (
         <Product product={product} key={product.id} />
       ))}
-      <Modal title="Create New Product">
-        <CreateProduct />
-      </Modal>
+      {modal && (
+        <Modal title="Create New Product" onClose={() => close()}>
+          <CreateProduct onCreate={createHandler} />
+        </Modal>
+      )}
+      <button
+        className="fixed bottom-5 right-5 rounded bg-red-700 text-white text-2xl py-2 px-4"
+        onClick={() => open()}
+      >
+        Add Item
+      </button>
     </div>
   );
 }
